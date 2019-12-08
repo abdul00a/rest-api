f991332b55be6ffd0d1abf0784175ff33fc1aacc
Abdul <abdul.ashraf@mountblue.tech>
2019-12-09 00:01:52 +0530

    Seed-data will be created for two entity autho and authorPosts

---
+++ src/utils/seedData.js
@@ -0,0 +1,101 @@
+const faker = require('faker');
+const { client } = require('./connection');
+
+function insertData1(value) {
+  return client.query('INSERT INTO postTbl VALUES ($1, $2, $3)', value);
+}
+
+function insertData(value) {
+  return client.query('INSERT INTO author VALUES ($1, $2, $3, $4)', value);
+}
+
+async function createTables(fakeData) {
+  try {
+    await client.query('DROP TABLE IF EXISTS author CASCADE');
+    await client.query(
+      'CREATE TABLE author (id INT PRIMARY KEY,authorName VARCHAR(30),authorEmail VARCHAR(60), numofpost INT)'
+    );
+    await Promise.all(
+      fakeData.map(ele =>
+        insertData([
+          fakeData.indexOf(ele) + 1,
+          ele.authorName,
+          ele.authorEmail,
+          ele.numofpost
+        ])
+      )
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function createTables1(data) {
+  try {
+    await client.query('DROP TABLE IF EXISTS postTbl CASCADE');
+    await client.query(
+      'CREATE TABLE postTbl (id INT,refID INT,posts VARCHAR(60))'
+    );
+    await Promise.all(
+      data.map((ele, i) => insertData1([i + 1, ele.refId, ele.postName]))
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function fakeAuthors(numOfFakeData) {
+  const fakeDetails = [];
+  try {
+    while (numOfFakeData) {
+      const data = {};
+      data.authorName = faker.fake('{{name.findName}}');
+      data.authorEmail = faker.fake('{{internet.email}}');
+      data.numofpost = Math.ceil(Math.random() * 5);
+      fakeDetails.push(data);
+      numOfFakeData--;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return fakeDetails;
+}
+
+async function fakePosts() {
+  const fakeDetails = [];
+  try {
+    let authorID = 1;
+    const count = await client.query('select * from author');
+    let numOFdata = count.rowCount;
+    while (numOFdata) {
+      const data = {};
+      const numPost = await client.query(
+        `select numofpost as post from author where id = ${authorID}`
+      );
+      let numPostVal = +numPost.rows[0].post;
+      while (numPostVal) {
+        data.refId = authorID;
+        data.postName = faker.random.words();
+        numPostVal--;
+        fakeDetails.push(data);
+      }
+      authorID++;
+      numOFdata--;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return fakeDetails;
+}
+
+async function main() {
+  try {
+    await client.connect();
+    await createTables(await fakeAuthors(5));
+    await createTables1(await fakePosts());
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
